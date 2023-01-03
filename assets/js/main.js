dbb81300d44c04ce5f7b386860451d2c81d1ccc7
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-03 14:17:10 +0100

    ende

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,3 +11,14 @@ let n = x + " " + y
 console.log(n)
 
 document.write(n)
+
+
+document.write(x + "World")
+
+let meinString = "Ich bin Erster"
+
+meinString += " Ich komme auf Platz zwei"
+console.log(meinString)
+
+document.write(meinString)
+
